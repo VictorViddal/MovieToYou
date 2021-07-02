2ade071c876acc875a538fa4145ddb125f67ced3
vidal <victor.matheus.o.vidal@gmail.com>
2021-07-02 04:01:13 -0400

    MovieDetails Cell add with autolayout

---
+++ MovieToYou/TableViewCells/MovieDetailCell.swift
@@ -0,0 +1,8 @@
+//
+//  MovieDetailCell.swift
+//  MovieToYou
+//
+//  Created by Victor Vidal on 02/07/21.
+//
+
+import Foundation
---
+++ MovieToYou/TableViewCells/MovieDetailTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  MovieDetailTableViewCell.swift
+//  MovieToYou
+//
+//  Created by Victor Vidal on 02/07/21.
+//
+
+import UIKit
+
+class MovieDetailTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieName: UILabel!
+    @IBOutlet weak var moviewLikes: UILabel!
+    @IBOutlet weak var movieViews: UILabel!
+    var didTapFavoriteButton: Bool = false
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    @IBAction func favorite(_ sender: UIButton) {
+        didTapFavoriteButton.toggle()
+        if didTapFavoriteButton{
+            sender.setImage(UIImage(systemName: "Heart.fill"), for: .normal)
+        } else {
+            sender.setImage(UIImage(systemName: "Heart"), for: .normal)
+        }
+    }
+    
+}
